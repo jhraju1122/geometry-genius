cd554b19b9bf6be16d8e0d6d9782c2d3f955a8e6
jhraju <jhraju890@gmail.com>
2024-02-13 23:14:10 +0600

    added triangle area full task finished

--- scripts/triangleCalculate.js
+++ scripts/triangleCalculate.js
@@ -6,25 +6,52 @@
 //step-5:convert the value to a number . use parseFloat
 
 function calculateTriangleArea(){
-    //get the triangle base value
-    const triangleBaseInput = document.getElementById('triangle-base');
-    const triangleBaseText = triangleBaseInput.value;
-    //eta dile cpu motherBord amra input dile pabe
-    const base = parseFloat(triangleBaseText)
-    console.log( base);
+    //triangle base value..
+ const triangleBaseInput = document.getElementById('traingle-base');
+ const triangleBaseText = triangleBaseInput.value ;
+ const base = parseFloat(triangleBaseText);
+//  console.log(base);
+// get triangle height value 
+const triangleHeightInput = document.getElementById('triangle-height');
+const triangleHeightText = triangleHeightInput.value;
+const height = parseFloat(triangleHeightText);
+// console.log(height);
 
+// calculate Triangle Area
+const area = 0.5 * base * height;
+console.log(area);
 
-    // get the triangle height value
-    const triangleHeightInput = document.getElementById('triangle-height');
-    const triangleHeightText  = triangleHeightInput.value;
-    const height = parseFloat(triangleHeightText)
-    console.log(height);
-
-    // calculate triangle area
-    const area = 0.5 * base * height;
-    console.log(area);
-
-    //display triangle area
-    const triangleAreaSpan = document.getElementById('triangle-area');
-    triangleAreaSpan.innerText = area;
+//display triangle area
+const triangleAreaSpan = document.getElementById('triangle-area');
+triangleAreaSpan.innerText = area;
 }
+
+
+
+
+
+
+
+// function calculateTriangleArea(){
+//     //get the triangle base value
+//     const triangleBaseInput = document.getElementById('triangle-base');
+//     const triangleBaseText = triangleBaseInput.value;
+//     //eta dile cpu motherBord amra input dile pabe
+//     const base = parseFloat(triangleBaseText)
+//     console.log( base);
+
+
+//     // get the triangle height value
+//     const triangleHeightInput = document.getElementById('triangle-height');
+//     const triangleHeightText  = triangleHeightInput.value;
+//     const height = parseFloat(triangleHeightText)
+//     console.log(height);
+
+//     // calculate triangle area
+//     const area = 0.5 * base * height;
+//     console.log(area);
+
+//     //display triangle area
+//     const triangleAreaSpan = document.getElementById('triangle-area');
+//     triangleAreaSpan.innerText = area;
+// }
